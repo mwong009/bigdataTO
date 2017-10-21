389d806d333dc2fb3a77e0a7032ff0db233d1ff1
melvin <melvin.wong@polymtl.ca>
2017-10-21 14:17:30 -0400

    add final time and save path

--- script_rbm.py
+++ script_rbm.py
@@ -1,5 +1,5 @@
-import timeit
-import pickle
+import os, sys, datetime
+import timeit, pickle
 import theano
 import pandas as pd
 import numpy as np
@@ -10,6 +10,13 @@ from ml.models import RestrictedBoltzmannMachine
 from ml.optimizers import sgd, rmsprop, adadelta, nesterov_momentum
 
 def main():
+
+    now = datetime.datetime.now()
+    path = sys.path[0] + '/' + str(now.date()) + '/'
+
+    if not os.path.exists(os.path.dirname(path)):
+	    os.makedirs(os.path.dirname(path))
+
     print('loading data...')
     f = open('dataset.save', 'rb')
     loadedObj, norms = pickle.load(f)
@@ -23,15 +30,19 @@ def main():
     print('training the model...')
 
     num_epochs = 100
-    rbm.initialize_session()
+    rbm.initialize_session(path)
     start_time = timeit.default_timer()
     while (rbm.epoch < num_epochs):
         rbm.one_train_step()
 
     end_time = timeit.default_timer()
+    timedelta = end_time - start_time
 
     print('The code ran for %d epochs with %.3f epochs/sec' %
-        (rbm.epoch, 1. * rbm.epoch / (end_time - start_time)))
+        (rbm.epoch, 1. * rbm.epoch / timedelta))
+
+    print('Total training time: %d h %d m %.2f s' %
+        (int(timedelta/3600), int(timedelta / 60) % 60, timedelta % 60))
 
 if __name__ == '__main__':
     main()
